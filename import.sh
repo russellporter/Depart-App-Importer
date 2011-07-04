8666264e3736948fde2e23d2db8e16a63b701d19
Russell Porter <contact@russellporter.com>
2011-07-03 18:42:22 -0700

    Re-enabled automatic gtfs file downloading

--- import.sh
+++ import.sh
@@ -7,12 +7,13 @@ export PATH=$PATH:/opt/local/lib/mysql5/bin
 rm settings.py
 echo "MYSQL_USER = '$MYSQL_USER'\nMYSQL_PASSWORD = '$MYSQL_PASSWORD'\nMYSQL_DATABASE = '$MYSQL_DATABASE'\nMYSQL_HOST = '$MYSQL_HOST'" > settings.py
 
-#rm -Rf gtfs/*
+mkdir gtfs
+rm -Rf gtfs/*
 cd gtfs
 
-#wget "$GTFS_URL" -O gtfs.zip
-#unzip gtfs.zip
-#rm gtfs.zip
+wget "$GTFS_URL" -O gtfs.zip
+unzip gtfs.zip
+rm gtfs.zip
 
 cat ../sql_better/load.sql | mysql --local-infile --password=$MYSQL_PASSWORD -u $MYSQL_USER -h $MYSQL_HOST -D $MYSQL_DATABASE
 
